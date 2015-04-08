2b5687625ea9f570bcfa9b5c0e531c748c5371fe
Darren Ackers <ackers86@hotmail.com>
2015-04-08 20:24:17 +0100

    Initial commit, umbraco added and home page in place

---
+++ CarpetsAtHome.Web/Gruntfile.js
@@ -0,0 +1,48 @@
+module.exports = function (grunt) {
+
+grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    compass: {
+      dist: {
+        options: {
+          config: 'config.rb'
+        }
+      }
+    },
+    watch: {
+      sass: {
+        files: ['sass/**/*.{scss,sass}','sass/_components/**/*.{scss,sass}'],
+        tasks: ['sass:dist']
+      },
+      livereload: {
+        files: ['*.html', '*.php', 'js/**/*.{js,json}', 'css/*.css','img/**/*.{png,jpg,jpeg,gif,webp,svg}'],
+        options: {
+          livereload: true
+        }
+      }
+    },
+    sass: {
+      dist: {
+        files: {
+          'public/stylesheets/main.css': 'client/sass/main.scss'
+        }
+      }
+    },
+    
+    bower_concat: {
+      all: {
+        dest: 'public/javascripts/bower.js'
+      }
+    }
+    });
+
+  //grunt.loadNpmTasks('grunt-bower-concat');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-compass');
+
+
+  // Default task(s).
+  grunt.registerTask('default', ['compass']);
+};
+	
